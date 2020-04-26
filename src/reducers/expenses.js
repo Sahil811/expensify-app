5050c445d9ef304fca732541c2519913bb066f93
unknown <sahilsiddiqui.dev@gmail.com>
2020-04-26 17:01:02 +0530

    Get firebase expenses

--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -19,7 +19,9 @@ export default  (state = erds, action) => {
                 } else {
                     return expense;
                 }
-            })    
+            });
+        case 'SET_EXPENSES':
+            return action.expenses;        
         default:
             return state;
     }
