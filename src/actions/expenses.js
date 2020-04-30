10a43a3bc7885a7436da291ad5d4fd3c9d36c2d9
unknown <sahilsiddiqui.dev@gmail.com>
2020-04-30 20:24:20 +0530

    Add Expensify Style

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -8,7 +8,8 @@ export const addExpense = (expense) => ({
 });
 
 export const startAddExpense = (expenseData = {}) => {
-    return (dispatch) => {
+    return (dispatch, getState) => {
+        const uid = getState().auth.uid
         const  {
             description = '',
             note = '',
@@ -17,7 +18,7 @@ export const startAddExpense = (expenseData = {}) => {
         } = expenseData;
         const expense = { description, note, amount, createdAt };
 
-        database.ref('expenses').push(expense).then((ref) => {
+        database.ref(`users/${uid}/expenses`).push(expense).then((ref) => {
             dispatch(addExpense({
                 id: ref.key,
                 ...expense
@@ -45,8 +46,9 @@ export const setExpenses = (expenses) => ({
 });
 
 export const startSetExpense = () => {
-    return (dispatch) => {     
-       return database.ref('expenses').once('value').then((snapshot) => {
+    return (dispatch, getState) => {     
+       const uid = getState().auth.uid
+       return database.ref(`users/${uid}/expenses`).once('value').then((snapshot) => {
             const expenses = [];
           
             snapshot.forEach((childSnapshot) => {
@@ -62,16 +64,18 @@ export const startSetExpense = () => {
 };
 
 export const startRemoveExpense  = ({ id } = {}) => {
-  return (dispatch) => {
-      return database.ref(`expenses/${id}`).remove().then((ref) => {
+  return (dispatch, getState) => {
+      const uid = getState().auth.uid
+      return database.ref(`users/${uid}/expenses/${id}`).remove().then((ref) => {
           dispatch(removeExpense({ id }));
       })
   }
 };
 
 export const startEditExpense  = (id, updates) => {
-  return (dispatch) => {
-      return database.ref(`expenses/${id}`).update(updates).then(() => {
+  return (dispatch, getState) => {
+      const uid = getState().auth.uid
+      return database.ref(`users/${uid}/expenses/${id}`).update(updates).then(() => {
           dispatch(editExpense(id , updates));
       })
   }
