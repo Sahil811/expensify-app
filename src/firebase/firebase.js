17c5b26c7850819c3358af42843538aa7b8d9032
Sahil811 <sahilsiddiqui.dev@gmail.com>
2021-04-19 16:39:54 +0530

    master: firebase stup

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,22 @@
 import * as firebase from 'firebase';
 
+// const config = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.FIREBASE_DATABASE_URL,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+// };
+
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
-};
+  apiKey: "AIzaSyAxuzt4Y4jJ6MEw4TRELibcDNb38Solaj0",
+  authDomain: "expensify-v101-test.firebaseapp.com",
+  databaseURL: "https://expensify-v101-test.firebaseio.com",
+  projectId: "expensify-v101-test",
+  storageBucket: "expensify-v101-test.appspot.com",
+  messagingSenderId: "571993528827",
+}
 // Initialize Firebase
 firebase.initializeApp(config);
 
